baf6bdda004d82fe49e006853886e39f6b033c35
Frida Sundberg <sundberg.fr@gmail.com>
2016-10-06 15:25:12 +0200

    Implemented and tested adder_ctrl (Lab 2)

--- lab2-3/asm/lab2/alu_test.asm
+++ lab2-3/asm/lab2/alu_test.asm
@@ -1,39 +1,68 @@
 	.code
 
 
-	;; TODO: test the 'add' instruction
-	;; ...
+	;; Test the 'add' instruction
+	set	r0,0xffff
+	set 	r1,1
+	nop
+	add 	r2,r0,r1 	; set carry flag to 1
+	nop
+	set 	r0,4
+	set 	r1,5
+	nop
+	add 	r2,r0,r1
+	nop
+	out 	0x11,r2		; should be 9 (0009)
 
 
-	;; TODO: test the 'addc' instruction
-	;; ...
+	;; Test the 'addc' instruction
+	set	r0,0xffff
+	set 	r1,1
+	nop
+	add 	r2,r0,r1 	; set carry flag to 1
+	nop
+	set 	r0,4
+	set 	r1,5
+	nop
+	addc 	r2,r0,r1	
+	nop
+	out 	0x11,r2		; should be 10 (000a)
 
 
-	;; TODO: test the 'sub' instruction
-	;; ...
+	;; Test the 'sub' instruction
+	set 	r0,4
+	set 	r1,5
+	nop
+	sub 	r2,r0,r1	
+	nop
+	out 	0x11,r2		; should be -1 (ffff)	
 
 
-	;; TODO: test the 'subc' instruction
-	;; ...
+	;; Test the 'subc' instruction
+	set 	r0,4
+	set 	r1,5
+	nop
+	subc 	r2,r0,r1	
+	nop
+	out 	0x11,r2		; should be -2 (fffe)
 
 
-	;; TODO: test the 'abs' instruction
-	;; ...
 
+	;; Test the 'abs' instruction
+	abs 	r0,r2
+	nop
+	out 	0x11,r0		; should be 2 (0002)
 
-	;; TODO: test the 'cmp' instruction
+	;; Test the 'cmp' instruction
 	set	r0,4
-	set	r1,2485
+	set	r1,4
 	nop
 	nop
 	cmp	r0,r1
 	nop
-	nop
 	move	r0,fl0		; read flags register
 	nop
-	nop
-	out	0x11,r0
-	;; ...
+	out	0x11,r0	
 
 
 	;; TODO: test the 'min' and 'max' instructions
--- lab2-3/rtl/adder_ctrl.v
+++ lab2-3/rtl/adder_ctrl.v
@@ -1,14 +1,26 @@
 
 module adder_ctrl
   (
-    input wire [2:0]    function_i,
-    input wire 		opa_sign_i,
-    output wire 	mx_opa_inv_o,
-    output wire [1:0] 	mx_ci_o
+   input wire [2:0]  function_i,
+   input wire 	     opa_sign_i,
+   output wire 	     mx_opa_inv_o,
+   output wire [1:0] mx_ci_o
    );
 
-   // Remove the following lines and put your code here
-   assign 		mx_opa_inv_o = 0;
-   assign 		mx_ci_o = 0;
+   reg [2:0] 	     mx_ctrl;
+   
+   always @(*) begin
+      case(function_i)
+	3'b000: mx_ctrl = 3'b000; // ADD
+	3'b001: mx_ctrl = 3'b010;// ADDC
+	3'b010, 3'b101: mx_ctrl = 3'b101;// SUB, CMP
+	3'b011: mx_ctrl = 3'b110; // SUBC
+	3'b100: mx_ctrl = {opa_sign_i,1'b0, opa_sign_i}; // ABS
+	default: mx_ctrl = 3'b000; // MAX, MIN (don't care)
+      endcase
+   end // always @(*)
 
+   assign mx_opa_inv_o = mx_ctrl[2];
+   assign mx_ci_o = mx_ctrl[1:0];
+   
 endmodule // adder_ctrl
