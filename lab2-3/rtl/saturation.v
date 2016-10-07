3a76121dd10f95d4529d8d102645cee44a3e33e1
Frida Sundberg <sundberg.fr@gmail.com>
2016-10-07 10:17:50 +0200

    Implemented and tested saturation for the mac unit (Lab 2)

--- lab2-3/rtl/saturation.v
+++ lab2-3/rtl/saturation.v
@@ -1,14 +1,25 @@
 
 module saturation
   (
-    input wire [39:0]   value_i,
-    input wire 		do_sat_i,
-    output wire [39:0] 	value_o,
-    output wire 	did_sat_o
+    input wire [39:0] value_i,
+    input wire 	      do_sat_i,
+    output reg [39:0] value_o,
+    output reg 	      did_sat_o
    );
 
-   // Remove the following lines and put your code here
-   assign 		value_o = value_i;
-   assign 		did_sat_o = 0;
+   always @(*) begin
+      case(value_i[39:31])
+	9'b000000000, 9'b111111111 : did_sat_o = 1'b0;
+	default : did_sat_o = do_sat_i & 1'b1;
+      endcase // case (value_i[39:31])
+   end // always @ begin
+   
+   always @(*) begin
+      case({did_sat_o,value_i[39]})
+	2'b10 : value_o = 40'h007fffffff;
+	2'b11 : value_o = 40'hff80000000;
+        default : value_o = value_i;
+      endcase // case (did_sat_o)
+   end // always @ begin
 
 endmodule // saturation
